fdc1dc6bd5b3c9a4f805f05a57faf886e6dc7271
Angiellll <161703500+Angiellll@users.noreply.github.com>
2024-05-09 18:00:24 +0800

    113.5.9

---
+++ 0509/SpinnerAdapter2/app/src/main/java/com/example/spinneradapter2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.spinneradapter2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner drinks,temp;
+    private TextView txv;
+    private String[] tempSet1 = {"冰","去冰","溫"};
+    private String[] tempSet2 = {"冰","去冰"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
