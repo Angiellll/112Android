5f58e650b07435d5ceff7d8ba2ab92cd9adfc5e8
Angiellll <161703500+Angiellll@users.noreply.github.com>
2024-05-23 17:26:23 +0800

    113.5.23

--- 0516/AlertDialogMultiChoiceDemo/app/src/main/java/com/example/alertdialogmultichoicedemo/MainActivity.java
+++ 0516/AlertDialogMultiChoiceDemo/app/src/main/java/com/example/alertdialogmultichoicedemo/MainActivity.java
@@ -7,26 +7,28 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener{
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener, DialogInterface.OnMultiChoiceClickListener{
 
-    private static String[] items = {"Samsung","OPPO","Apple","ASUS"};
-    private static boolean[] itemsChecked = {false,true,false,true};
+    private static String[] items = {"Samsung", "OPPO", "Apple", "ASUS"};
+    private static boolean[] itemsChecked = {false, false, false, false};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btnSelect = (Button) findViewById(R.id.btnSelect);
-        btnSelect.setOnClickListener(new View.OnClickListener(){
+        btnSelect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 AlertDialog build = new AlertDialog.Builder(MainActivity.this)
                         .setTitle("請勾選選項?")
-                        .setPositiveButton("確定", null)
-                        .setNegativeButton("取消", null)
-                        .setMultiChoiceItems(items,itemsChecked,null)
+                        .setPositiveButton("確定", MainActivity.this)
+                        .setNegativeButton("取消", MainActivity.this)
+                        .setMultiChoiceItems(items, itemsChecked, MainActivity.this)
                         .show();
             }
         });
@@ -35,13 +37,26 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
     @Override
     public void onClick(DialogInterface dialog, int which) {
         String msg = "";
-        switch(which){
+        switch (which) {
             case DialogInterface.BUTTON_POSITIVE:
-                for(int index=0; index<items.length; index++){
-                    if(itemsChecked[index]){
+                for (int index = 0; index < items.length; index++) {
+                    if (itemsChecked[index])
                         msg += items[index] + "\n";
-                    }
                 }
-        }
+                TextView output = (TextView) findViewById(R.id.lblOutput);
+                output.setText(msg);
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                Toast.makeText(this, "按下取消紐!", Toast.LENGTH_SHORT).show();
+                break;
+       }
+    }
+
+
+
+
+    @Override
+    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+        Toast.makeText(this, items[which] + (isChecked ? " 勾選" : "取消勾選"), Toast.LENGTH_SHORT).show();
     }
 }
