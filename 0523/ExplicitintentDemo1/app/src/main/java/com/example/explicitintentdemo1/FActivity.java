d4ca34052e01f494584077befff9e9c77c015a08
Angiellll <161703500+Angiellll@users.noreply.github.com>
2024-05-23 17:28:56 +0800

    113.5.23

--- 0523/ExplicitintentDemo1/app/src/main/java/com/example/explicitintentdemo1/FActivity.java
+++ 0523/ExplicitintentDemo1/app/src/main/java/com/example/explicitintentdemo1/FActivity.java
@@ -3,6 +3,8 @@ package com.example.explicitintentdemo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FActivity extends AppCompatActivity {
@@ -13,6 +15,15 @@ public class FActivity extends AppCompatActivity {
         setContentView(R.layout.activity_factivity);
 
         converTempture();
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
     }
 
     private void converTempture(){
